12a8c8a428e75ad0192fba4d6b39724895cb9c57
kodama.kcc <kodama.kcc@om.asahi-kasei.co.jp>
2018-03-05 18:49:48 +0900

    refactor main

--- src/main.py
+++ src/main.py
@@ -22,12 +22,13 @@ class StateMachine(object):
     __VECT_LEN_TH = 0.8
     __DEBUG_TIMER_FREQ_SEC = 0.2
     __AUDIO_NAME = 'tanuki.wav'
-    __AUDIO = audio_player.AudioPlayerPygame(__AUDIO_NAME)
-    __CAPTURE = cv2.VideoCapture(0)
-    __DETECTOR = detector.Detector(__COLOR_VECT, __VECT_LEN_TH)
-
 
     def __init__(self):
+        self.__audio = audio_player.AudioPlayerPygame(StateMachine.__AUDIO_NAME)
+        self.__capture = cv2.VideoCapture(0)
+        self.__detector = detector.Detector(StateMachine.__COLOR_VECT,
+                                            StateMachine.__VECT_LEN_TH)
+
         self.__state_key = 'sleeping'
         self.__sleeping_proc = (self.__switch_proc_sleeping,
                                 self.__null_proc,
@@ -50,28 +51,28 @@ class StateMachine(object):
                         self.__indicating_proc)
 
         self.__event_buf = []
-        self.__CAPTURE.set(3,320)
-        self.__CAPTURE.set(4,240)
-        if not self.__CAPTURE:
+        self.__capture.set(3,320)
+        self.__capture.set(4,240)
+        if not self.__capture:
             print("Could not open camera")
             sys.exit()
 
 
     def __push_event(self, event_key):
         print('pushed event:', event_key)
-        if event_key not in self.__EVENT_KIND:
+        if event_key not in StateMachine.__EVENT_KIND:
             print('[push_event error] no such event:', event_key)
             return self.__RET_FAIL
 
         self.__event_buf.append(event_key)
-        return self.__RET_SUCCESS
+        return StateMachine.__RET_SUCCESS
 
     def __null_proc(self):
         pass
 
     def __switch_proc_sleeping(self):
         self.__state_key = 'detecting'
-        timer = threading.Timer(self.__DETECT_FREQ_SEC,
+        timer = threading.Timer(StateMachine.__DETECT_FREQ_SEC,
                                 self.__push_event,
                                 args=['timer'])
 
@@ -79,26 +80,26 @@ class StateMachine(object):
 
     def __switch_proc_detecting_indicating(self):
         self.__state_key = 'sleeping'
-        self.__AUDIO.stop()
+        self.__audio.stop()
 
     def __timer_proc(self):
-        _, img = self.__CAPTURE.read()
-        self.__DETECTOR.input_img(img)
+        _, img = self.__capture.read()
+        self.__detector.input_img(img)
 
-        if self.__DETECTOR.detect():
+        if self.__detector.detect():
             self.__push_event('detected_full')
             print('detected full!!')
-            self.__DETECTOR.showResult()
+            self.__detector.showResult()
 
         else:
             self.__push_event('detected_not_full')
 
     def __detected_full_proc(self):
         self.__state_key = 'indicating'
-        self.__AUDIO.play(loop=True)
+        self.__audio.play(loop=True)
 
     def __detected_not_full_proc(self):
-        timer = threading.Timer(self.__DETECT_FREQ_SEC,
+        timer = threading.Timer(StateMachine.__DETECT_FREQ_SEC,
                                 self.__push_event,
                                 args=['timer'])
 
@@ -106,23 +107,23 @@ class StateMachine(object):
 
 
     def __proc(self, event_key):
-        if event_key not in self.__EVENT_KIND:
-            return self.__RET_FAIL
+        if event_key not in StateMachine.__EVENT_KIND:
+            return StateMachine.__RET_FAIL
 
-        self.__procs[self.__STATE_KIND[self.__state_key]][self.__EVENT_KIND[event_key]]()
+        self.__procs[StateMachine.__STATE_KIND[self.__state_key]][StateMachine.__EVENT_KIND[event_key]]()
 
-        return self.__RET_SUCCESS
+        return StateMachine.__RET_SUCCESS
 
     def __debug_print_state_loop(self):
         print('state:', self.__state_key)
         print('event_num:', len(self.__event_buf))
 
-        timer_debug = threading.Timer(self.__DEBUG_TIMER_FREQ_SEC,
+        timer_debug = threading.Timer(StateMachine.__DEBUG_TIMER_FREQ_SEC,
                                       self.__debug_print_state_loop)
         timer_debug.start()
 
     def start_wait_event_loop(self):
-        timer_debug = threading.Timer(self.__DEBUG_TIMER_FREQ_SEC,
+        timer_debug = threading.Timer(StateMachine.__DEBUG_TIMER_FREQ_SEC,
                                       self.__debug_print_state_loop)
 
         timer_debug.start()
