df1a7491007204ff877380cb3506f22a872b90fc
kodama.kcc <kodama.kcc@om.asahi-kasei.co.jp>
2018-03-07 22:07:35 +0900

    update readConfig()

--- src/util.py
+++ src/util.py
@@ -3,16 +3,24 @@
 import csv
 
 def readConfig():
-    calib_data = {}
-    with open('../config.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            for col in range(len(row[1:])):
-                calib_data[row[0]] = [int(col) for col in row[1:]]
+    calib_data = {"angle":[0],
+                  "level":[0],
+                  "area":[0, 0, 0, 0]}
 
-        print('calib_data =', calib_data)
+    try:
+        with open('../config.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                for col in range(len(row[1:])):
+                    calib_data[row[0]] = [int(col) for col in row[1:]]
 
-        return calib_data
+    except FileNotFoundError:
+        print('config.csv not found. return default falue!')
+        calib_data
+
+    print('calib_data =', calib_data)
+
+    return calib_data
 
 def transPosCenterBase(x=None, y=None, center_x=None, center_y=None):
     ret_x = None
