f4e35e5ecf8491aa1f0fa3830429b4625aa5c47d
kodama.kcc <kodama.kcc@om.asahi-kasei.co.jp>
2018-03-15 21:19:26 +0900

    update clientimg.py

--- src/clientimg.py
+++ src/clientimg.py
@@ -5,30 +5,43 @@ import numpy
 import cv2
 
 
-def getImageSocket():
-    #IPアドレスとポート番号は環境に応じて変更
+class ClientImg(object):
+    #    HOST = "192.168.1.100"
+    #    PORT = 8000
+
+    def __init__(self, host, port):
+        self.__host = host
+        self.__port = port
+        self.__socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    def connect(self):
+        self.__socket.connect((self.__host, self.__port))
+
+    def getImageSocket(self):
+        #IPアドレスとポート番号は環境に応じて変更
+        sended_bytes = self.__socket.send(b'HELLO\n')
+        print('message send:', sended_bytes)
+        
+        buf=b""
+        recvlen=100
+        while recvlen > 0:
+            receivedbytes=self.__socket.recv(1024*8)
+            recvlen=len(receivedbytes)
+            buf +=receivedbytes
+        #    narray=numpy.fr/omstring(buf,dtype='uint8')
+
+        narray=numpy.frombuffer(buf,dtype='uint8')
+        print('bufsize =', len(buf))
+        return cv2.imdecode(narray,1)
+
+if __name__ == '__main__':
 #    HOST = "192.168.1.100"
     HOST = "localhost"
     PORT = 8000
-    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((HOST,PORT))
-    sock.send(b'HELLO\n')
-    buf=b""
-    recvlen=100
-    while recvlen>0:
-        receivedbytes=sock.recv(1024*8)
-        recvlen=len(receivedbytes)
-        buf +=receivedbytes
-    sock.close()
-#    narray=numpy.fromstring(buf,dtype='uint8')
-    narray=numpy.frombuffer(buf,dtype='uint8')
-    return cv2.imdecode(narray,1)
 
-if __name__ == '__main__':
-#    while True:
-    img = getImageSocket()
-    cv2.imshow('Capture',img)
-    cv2.waitKey(0)
-    
-#    if cv2.waitKey(100) & 0xFF == ord('q'):
-#       break
+    while True:
+        client_img = ClientImg(HOST, PORT)
+        client_img.connect()
+        img = client_img.getImageSocket()
+        cv2.imshow('Capture',img)
+        cv2.waitKey(0)
