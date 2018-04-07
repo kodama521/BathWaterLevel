7f00d0c517cd6633782cfe4307edc7af8709597f
kodama.kcc <kodama.kcc@om.asahi-kasei.co.jp>
2018-04-07 15:26:47 +0900

    add switch_ctrl.py

---
+++ src/switch_ctrl.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as gpio
+
+def sw_set_callback(pin, callback, event_type=gpio.FALLING):
+    gpio.setmode(gpio.BCM)
+    gpio.setup(pin, gpio.IN, pull_up_down=GPIO.PUD_UP)
+    gpio.add_event_detect(pin, event_type, callback=callback, bouncetime=200)
+
+
+def debug_callback(pin):
+    print('no.',pin)
+    print('pin is pushed!')
+
+if __name__ == '__main__':
+    sw_set_callback(20, debug_callback)
